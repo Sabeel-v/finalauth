cf8df56723c7cf8b69bb67818abbf9a45fc2d54d
sabeel-v <sabeelv825@gmail.com>
2025-09-06 13:17:44 +0530

    third commit

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://api.sabeelvmukkam.shop",
+  baseURL: "https://api.sabeelvmukkam.shop",
   withCredentials: true,
 });
 
